88efbf1e4a02fe7672db5aee464f5bc5bcebea00
doit-zero <131241458+doit-zero@users.noreply.github.com>
2025-06-18 22:05:12 +0900

    main 영역 구현

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,37 @@
 <script setup>
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+import {useAccountStore} from "@/stores/account.js";
+import {watch} from "vue";
+import {useRoute} from "vue-router";
+
+// 계정 스토어
+const accountStore = useAccountStore();
+
+// 라우트 객체: VueRouter에서 제공하는 컴포지션 API 함수로, 현재 활성화된 라우트의 정보를 가져오는데 사용됨
+const route = useRoute();
+
+// 로그인 여부 확인
+const checkAccount = async => {
+  // 추후 구현
+};
+
+// 커스텀 생성 훅
+(async function onCreated(){
+  await checkAccount();
+})();
+
+// 라우트 경로가 바뀔 때마다 로그인 여부를 확인
+watch(()=> route.path, () => {
+  checkAccount();
+});
+
 </script>
 
 <template>
   <Header />
+  <mai>
+    <router-view></router-view>
+  </mai>
   <Footer />
 </template>
