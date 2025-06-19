2d329d966a61543aab78cc82adfd692b064b6408
doit-zero <131241458+doit-zero@users.noreply.github.com>
2025-06-19 20:47:23 +0900

    로그인 기능 구현

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -12,6 +12,11 @@ const router = createRouter({
       path: '/join',
       name: 'join',
       component: () => import('../views/Join.vue')
+    },
+    {
+      path: '/login',
+      name: 'loin',
+      component: () => import('../views/Login.vue')
     }
   ]
 })
