19de37e225d07f77ab20bc42ab0762a5e805c53e
doit-zero <131241458+doit-zero@users.noreply.github.com>
2025-06-19 21:22:28 +0900

    상품 목록 보기 기능 구현

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,32 @@
 <script setup>
+import {getItems} from "@/services/itemService.js";
+import {reactive} from "vue";
+import Card from "@/views/Card.vue";
 
+// 반응형 상태
+const state = reactive({
+  item: []
+});
+// 커스텀 생성 훅 onCreated 훅을 사용하여 라이프사이클 훅 중에 가장 먼저 실행되도록 함
+(async function onCreated() {
+  const res =  await getItems();
+
+  if(res.status === 200){
+    state.item = res.data;
+  }
+})();
 </script>
 
 <template>
-  <div class="container">
-    <h1 class="mt-5">Hello~ Home</h1>
+  <div class="home">
+    <div class="album py-5 bg-light">
+      <div class="container">
+        <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3 g-3">
+          <div class="col" v-for="item in state.item">
+            <Card :item="item"/>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
