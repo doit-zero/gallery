853cc1cbb70ec96d620cb80be0ae5b0390aa6914
doit-zero <131241458+doit-zero@users.noreply.github.com>
2025-06-23 21:05:57 +0900

    Axios 인터셉터 적용

--- frontend/src/services/cartService.js
+++ frontend/src/services/cartService.js
@@ -1,19 +1,19 @@
-import axios from "axios";
+import httpRequester from "@/lib/httpRequester.js";
 /**
  * 이 서비스는 백엔드 API를 호출해서 장바구니의 데이터를 처리하는 기능을 제공함
  *
  * */
 // 장바구니 상품 목록 조회
 export const getItems = () => {
-    return axios.get("/v1/api/cart/items").catch(e=>e.response);
+    return httpRequester.get("/v1/api/cart/items").catch(e=>e.response);
 }
 
 // 장바구니 상품 추가
 export const addItem = (itemId) => {
-    return axios.post("/v1/api/carts",{itemId}).catch(e=>e.response);
+    return httpRequester.post("/v1/api/carts",{itemId}).catch(e=>e.response);
 }
 
 // 장바구니에서 상품 삭제
 export const removeItem = (itemId) => {
-    return axios.delete(`/v1/api/cart/items/${itemId}`).catch(e=>e.response);
+    return httpRequester.delete(`/v1/api/cart/items/${itemId}`).catch(e=>e.response);
 }
--- frontend/src/services/itemService.js
+++ frontend/src/services/itemService.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import httpRequester from "@/lib/httpRequester.js";
 /**
  * 이 서비스는 백엔드 API를 호출해서 상품목록 데이터를 처리하는 기능을 제공함
  *
  * */
 // 상품 목록 조회
 export const getItems = () => {
-    return axios.get("/v1/api/items").catch(e=>e.response);
+    return httpRequester.get("/v1/api/items").catch(e=>e.response);
 }
