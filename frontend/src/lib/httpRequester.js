38f45a7676f0e31e1c0d94fd0a7ff91c2fda0e24
DoubleJ <131241458+doit-zero@users.noreply.github.com>
2025-06-25 22:42:32 +0900

    http 요청 라이브러리 수정

--- frontend/src/lib/httpRequester.js
+++ frontend/src/lib/httpRequester.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import {useAccountStore} from "@/stores/account.js";
 
 const instance = axios.create();
 
@@ -11,7 +12,32 @@ instance.interceptors.response.use((res) => {
             window.alert("잘못된 요청입니다.");
             break;
         case 401:
-            window.alert("권한이 없습니다.");
+            const config = err.config;
+            if(config.retried){
+                window.alert("권한이 없습니다.");
+                window.location.replace("/");
+                return ;
+            }
+            // (쿠키에 있는) 리프레시 토큰으로 액세스 토큰 요청
+            const res = await axios.get("v1/api/account/token");
+
+            // 액세스 토큰
+            const accessToken = res.data;
+
+            // 계정 스토어
+            const accountStore = useAccountStore();
+
+            // 계정 스토어의 액세스 토큰 변경
+            accountStore.setAccessToken(useAccountStore);
+
+            //요청 액세스 토큰 교체
+            config.headers.authorization = `Bearer ${accountStore.accessToken}`;
+
+            //중복 재요청 방지를 위한 프로퍼티 추가
+            config.retried = true;
+
+            // 재요청
+            return instance(config);
             break;
         case 500:
             window.alert("오류가 있습니다. 관리자에게 문의해주세요.");
@@ -21,17 +47,33 @@ instance.interceptors.response.use((res) => {
     return Promise.reject(err);
 });
 
+// http 요청 설정 생성
+const generateConfig = () => {
+  // 계정 스토어
+  const accountStore = useAccountStore();
+
+  if(accountStore.accessToken){
+      return {
+          headers: {authorization: `Bearer ${accountStore.accessToken}`}
+      };
+  }
+
+  return {};
+};
+
 export default {
     get(url,params){
-        return instance.get(url,{params});
+        const config = generateConfig();
+        config.params = params;
+        return instance.get(url,config);
     },
     post(url,params){
-        return instance.post(url,{params});
+        return instance.post(url,generateConfig());
     },
     put(url,params){
-        return instance.put(url,{params});
+        return instance.put(url,generateConfig());
     },
     delete(url,params){
-        return instance.delete(url,{params});
+        return instance.delete(url,generateConfig());
     }
 }
