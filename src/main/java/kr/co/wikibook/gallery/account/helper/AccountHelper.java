1d3620318f323decd2bd98e0e0df563e2f893d96
doit-zero <131241458+doit-zero@users.noreply.github.com>
2025-06-30 23:15:47 +0900

    패키지 변경

--- src/main/java/kr/co/wikibook/gallery/account/helper/AccountHelper.java
+++ src/main/java/kr/co/wikibook/gallery/account/helper/AccountHelper.java
@@ -1,4 +1,4 @@
-package kr.co.wikibook.gallery.member.helper;
+package kr.co.wikibook.gallery.account.helper;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
