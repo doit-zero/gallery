bdd10f9875fbd668e2e9e212daa03baedb0e1049
doit-zero <131241458+doit-zero@users.noreply.github.com>
2025-07-01 20:50:21 +0900

    정규 표현식을 이용한 정적 파일 포워드
    문제: 새로고침을 하면 서버는 404를 내려줌
    해결: 정규 표현식을 이용한 정적 파일 포워드 방식으로 해결함

--- src/main/java/kr/co/wikibook/gallery/common/controller/MainController.java
+++ src/main/java/kr/co/wikibook/gallery/common/controller/MainController.java
@@ -7,7 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MainController {
-    @GetMapping("/")
+
+    /**
+     * 정규 표현식을 이용한 정적 파일 포워드
+     * SPA 와 같은 프론트엔드 라우팅을 사용하는 앱에서는 브라우저가 새로고침 하거나 직접 URL 접근 시 서버가 해당 경로를 처리하려다 404가 나는 것을 방지하고
+     * -> 무조건 /index.html을 반환해서 프론트엔드 라우터가 이어받도록 하는 패턴
+     * @GetMapping(value = {
+     *     "/",                         // 루트 요청
+     *     "{path:[^.]*}",              // `/abc` 처럼 .이 없는 단일 경로
+     *     "{path1:[^.]*}/{path2:[^.]*}" // `/abc/def` 처럼 .이 없는 2단계 경로
+     * }) 
+     * */
+    @GetMapping(value = {"/","{path:[^.]*}","{path1:[^.]*}/{path2:[^.]*}"})
     public String home(){
         return "/index.html";
     }
