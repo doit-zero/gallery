64e38a9e887d281a9f9bf51201c0183984af3c95
doit-zero <131241458+doit-zero@users.noreply.github.com>
2025-07-10 22:17:39 +0900

    Coupon Dto
    - toCouponEntity()
    dto를 바로 entity로 변환되어 coupon 반환하도록 구현 ,
    coupon entity에는 @Builder를 사용함, 왜냐하면 첫번째로는 추후 코드 Dto요구 사항 변경시에도 해당 메서드만 변경하면 되기 때문에 유지보수가 용이할 것이라 판단
    그리고 보기에도 깔끔
    
    -generateCode()
    name 과 UUID 랜덤 값과 조합되어  12자리 수를 만들어내는 용도

---
+++ src/main/java/kr/co/wikibook/gallery/coupon/dto/CouponCreateRequest.java
@@ -0,0 +1,57 @@
+package kr.co.wikibook.gallery.coupon.dto;
+
+import jakarta.persistence.*;
+import kr.co.wikibook.gallery.coupon.entity.Coupon;
+import kr.co.wikibook.gallery.coupon.entity.DiscountType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static kr.co.wikibook.gallery.coupon.entity.DiscountType.FIXED;
+
+@Getter
+@Setter
+@ToString
+public class CouponCreateRequest {
+    private String code;
+
+    private String name;
+
+    private DiscountType discountType;
+
+    private int discountValue;
+
+    private int totalQuantity;
+
+    private LocalDateTime validFrom;
+
+    private LocalDateTime validTo;
+
+    private LocalDateTime createdAt;
+
+    public Coupon toCouponEntity() {
+        return Coupon.builder()
+                .code(generateCode())
+                .name(this.name)
+                .discountType(this.discountType)
+                .discountValue(this.discountValue)
+                .totalQuantity(this.totalQuantity)
+                .validFrom(this.validFrom)
+                .validTo(this.validTo)
+                .build();
+    }
+
+    ;
+
+    private String generateCode() {
+        String base = name != null ? name.replaceAll("[^A-Za-z0-9]", "").toUpperCase() : "COUPON";
+        String prefix = base.length() > 4 ? base.substring(0, 4) : String.format("%-4s", base).replace(' ', 'X');
+        String random = UUID.randomUUID().toString().replaceAll("-", "").substring(0, 8).toUpperCase();
+        return prefix + random; // 총 12자리
+    }
+
+}
