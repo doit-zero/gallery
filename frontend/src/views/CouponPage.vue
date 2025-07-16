7f081cefd1381e96df6f1bf0ba801e2c57406aa5
doit-zero <131241458+doit-zero@users.noreply.github.com>
2025-07-16 22:59:07 +0900

    CouponPage 생성
    - CouponPage 생성
    : 발급 가능한 모든 쿠폰을 보는 페이지

---
+++ frontend/src/views/CouponPage.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+// 반응형 상태
+import {reactive} from "vue";
+import Card from "@/views/Card.vue";
+import Coupon from "@/views/Coupon.vue";
+import {getCoupons} from "@/services/couponService.js";
+
+const state = reactive({
+  coupons: []
+});
+
+(async function onCreated() {
+  const res = await getCoupons();
+
+  if(res.status === 200){
+    state.coupons = res.data;
+  }
+})();
+
+</script>
+
+<template>
+  <div class="coupons">
+    <div class="album py-5 bg-light">
+      <div class="container">
+        <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3 g-3">
+          <div class="col" v-for="coupon in state.coupons">
+            <Coupon :coupon="coupon"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
