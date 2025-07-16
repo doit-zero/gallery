b860eb4cb5f124eae008bbaf5ffe64694f42aaeb
doit-zero <131241458+doit-zero@users.noreply.github.com>
2025-07-16 22:57:13 +0900

    CouponPage 이동 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -37,6 +37,11 @@ const router = createRouter({
       path: '/orders/:id',
       name: 'orderDetail',
       component: () => import('../views/OrderDetail.vue')
+    },
+    {
+      path: '/coupons',
+      name: 'coupons',
+      component: () => import('../views/CouponPage.vue')
     }
   ]
 })
