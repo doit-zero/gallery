b05b7ead5196693ab649c87525750efbd964b142
doit-zero <131241458+doit-zero@users.noreply.github.com>
2025-07-16 23:03:17 +0900

    메서드명,반환타입 수정
    - 메서드명 수정
    - 반환타입 수정

--- src/main/java/kr/co/wikibook/gallery/coupon/service/CouponService.java
+++ src/main/java/kr/co/wikibook/gallery/coupon/service/CouponService.java
@@ -1,14 +1,15 @@
 package kr.co.wikibook.gallery.coupon.service;
 
 import kr.co.wikibook.gallery.coupon.dto.CouponCreateRequest;
+import kr.co.wikibook.gallery.coupon.dto.CouponResponse;
 import kr.co.wikibook.gallery.coupon.entity.Coupon;
 
 import java.util.List;
 
 public interface CouponService {
-    // 쿠폰 발급
-    Coupon issue(CouponCreateRequest request);
+    // 쿠폰 생성
+    Coupon create(CouponCreateRequest request);
 
-    // 모든 쿠폰 가져오기
-    List<Coupon> findAll();
+    // 사용가능한 쿠폰들 모두 가져오기
+    List<CouponResponse> findAll();
 }
