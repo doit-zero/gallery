86177836d17b94eb82e6b1b0fb3c3d48565eb1f9
doit-zero <131241458+doit-zero@users.noreply.github.com>
2025-07-17 23:23:14 +0900

    findAll 수정

--- src/main/java/kr/co/wikibook/gallery/coupon/service/CouponServiceImpl.java
+++ src/main/java/kr/co/wikibook/gallery/coupon/service/CouponServiceImpl.java
@@ -1,11 +1,13 @@
 package kr.co.wikibook.gallery.coupon.service;
 
 import kr.co.wikibook.gallery.coupon.dto.CouponCreateRequest;
+import kr.co.wikibook.gallery.coupon.dto.CouponResponse;
 import kr.co.wikibook.gallery.coupon.entity.Coupon;
 import kr.co.wikibook.gallery.coupon.respository.CouponRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,14 +16,26 @@ public class CouponServiceImpl implements CouponService{
     private final CouponRepository couponRepository;
 
     @Override
-    public Coupon issue(CouponCreateRequest request) {
+    public Coupon create(CouponCreateRequest request) {
         Coupon coupon = request.toCouponEntity();
         Coupon savedCoupon = couponRepository.save(coupon);
         return savedCoupon;
     }
 
+
     @Override
-    public List<Coupon> findAll() {
-        return couponRepository.findAll();
+    public List<CouponResponse> findAll() {
+        List<CouponResponse> couponResponseList = new ArrayList<>();
+        List<Coupon> couponList = couponRepository.findAll();
+
+        if(!couponList.isEmpty()){
+            for (Coupon coupon : couponList) {
+                CouponResponse couponResponse = coupon.toCouponResponse();
+                couponResponseList.add(couponResponse);
+            }
+            return couponResponseList;
+        }
+
+        return couponResponseList;
     }
 }
