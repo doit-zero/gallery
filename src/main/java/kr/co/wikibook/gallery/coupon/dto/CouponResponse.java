029607a929834f3383c3f5c9704ad36e79477c9c
doit-zero <131241458+doit-zero@users.noreply.github.com>
2025-07-17 23:22:31 +0900

    Response Dto
    - 쿠폰 응답 데이터 전송 객체

---
+++ src/main/java/kr/co/wikibook/gallery/coupon/dto/CouponResponse.java
@@ -0,0 +1,22 @@
+package kr.co.wikibook.gallery.coupon.dto;
+
+import kr.co.wikibook.gallery.coupon.entity.DiscountType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Builder
+public class CouponResponse {
+    private String code;
+    private String name;
+    private DiscountType discountType;
+    private Integer discountValue;
+    private LocalDateTime validFrom;
+    private LocalDateTime validTo;
+}
