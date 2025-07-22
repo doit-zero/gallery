59462d4c922bc9550ef826e2c623680eb136182d
doit-zero <131241458+doit-zero@users.noreply.github.com>
2025-07-22 21:46:03 +0900

    쿠폰 관련 상수

---
+++ src/main/java/kr/co/wikibook/gallery/coupon/CouponConstant.java
@@ -0,0 +1,7 @@
+package kr.co.wikibook.gallery.coupon;
+
+public abstract class CouponConstant {
+    public static final String COUPON_NOT_FOUND = "쿠폰을 찾을 수 없습니다.";
+    public static final String COUPON_EXHUASTION = "쿠폰이 모두 소진 되었습니다.";
+    public static final String ISSUE_SUCCESS = "쿠폰 발급 성공";
+}
